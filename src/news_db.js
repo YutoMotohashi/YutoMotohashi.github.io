0f78ae90e9a4944f61976eb40d8568e026260326
Yuto Motohashi <yuto.motohas@gmail.com>
2024-04-08 11:49:07 +0900

    update

--- src/news_db.js
+++ src/news_db.js
@@ -13,9 +13,17 @@ const newsItems = [
       date: "2024-02-01",
       link: "",
       title_en: "New Paper",
-      description_en: "",
+      description_en: "'Multi-zone trapped-ion qubit control in an integrated photonics QCCD device'",
       title_ja: "論文を公開しました。",
-      description_ja: ""
+      description_ja: "'Multi-zone trapped-ion qubit control in an integrated photonics QCCD device'"
+    }, 
+    {
+      date: "2024-04-08",
+      link: "",
+      title_en: "Graduate Program",
+      description_en: "From September 2024, I will be enrolled in master of quantum engineering at ETH Zurich",
+      title_ja: "進学先について",
+      description_ja: "ETH ZurichのMaster of Quantum Engineeringに2024年9月から進学することになりました。"
     }
   ];
   
