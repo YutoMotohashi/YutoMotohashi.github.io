c26b1c61c22bdb6bed751b2214f1674cfc1bb500
Yuto Motohashi <yuto.motohas@gmail.com>
2024-04-21 15:28:36 +0900

    change news order

--- src/app/data/experience.js
+++ src/app/data/experience.js
@@ -1,48 +1,48 @@
 export const scientificExperienceItems = [
-    {
-        index: 3,
-        start_date: `2022-04`,
-        end_date: `2024-03`,
-        institution_en: `University of Tokyo, Faculty of Engineering, School of Applied Physics`,
-        institution_ja: `東京大学工学部物理工学科`,
-        title_en: `Senior Thesis`,
-        title_ja: `卒業研究`,
-        description_en: `Supervisor: Kosuke Yoshioka
-Efficient detection of Ionized Positronium using MCP through Electromagnetic Field Simulations and Modifications`,
-        description_ja: `指導教員：吉岡孝高
-電磁場シミュレーションと電極設計による、イオン化ポジトロニウムの効率的な検出方法の開発`,
-        link: `http://www.fs.t.u-tokyo.ac.jp/index.html`
-    },
-    {
-        index: 2,
-        start_date: `2023-07`,
-        end_date: `2023-09`,
-        institution_en: `Cornell University, Electrical and Computer Engineering Department
-Photonics and Quantum Electronics Group`,
-        institution_ja: `コーネル大学 Electrical and Computer Engineering Department,
-Photonics and Quantum Electronics Group`,
-        title_en: `Internship`,
-        title_ja: `インターンシップ`,
-        description_en: `Supervisor: Karan Mehta
-Designed and simulated photonic crystal UV resonators in a novel platform via full 3D FDTD simulations`,
-        description_ja: `指導教員：Karan Mehta
-紫外線領域での、フォトニック結晶光共振器の設計と3D-FDTDシミュレーション`,
-        link: `https://sites.coecis.cornell.edu/mehta/`
-    },
-    {
-        index: 1,
-        start_date: `2022-09`,
-        end_date: `2023-06`,
-        institution_en: `ETH Zurich, Department of Physics, Trapped Ion Quantum Information Group`,
-        institution_ja: `スイス連邦工科大学チューリッヒ校 Department of Physics, Trapped Ion Quantum Information Group`,
-        title_en: `Research Project`,
-        title_ja: `研究プロジェクト`,
-        description_en: `Supervisor: Jonathan Home
-Transportation of Trapped-ion qubit in an integrated photonics QCCD device through optimization of electric fields and study of stray electric fields`,
-        description_ja: `指導教員：Jonathan Home
-電場の最適化と迷電場の研究を通じた、集積光学系を組み込んだQCCDデバイス内でのイオンの輸送`,
-        link: `https://tiqi.ethz.ch/`
-    },
+        {
+                index: 1,
+                start_date: `2022-09`,
+                end_date: `2023-06`,
+                institution_en: `ETH Zurich, Department of Physics, Trapped Ion Quantum Information Group`,
+                institution_ja: `スイス連邦工科大学チューリッヒ校 Department of Physics, Trapped Ion Quantum Information Group`,
+                title_en: `Research Project`,
+                title_ja: `研究プロジェクト`,
+                description_en: `Supervisor: Jonathan Home
+                Transportation of Trapped-ion qubit in an integrated photonics QCCD device through optimization of electric fields and study of stray electric fields`,
+                description_ja: `指導教員：Jonathan Home
+                電場の最適化と迷電場の研究を通じた、集積光学系を組み込んだQCCDデバイス内でのイオンの輸送`,
+                link: `https://tiqi.ethz.ch/`
+        },
+        {
+                index: 2,
+                start_date: `2023-07`,
+                end_date: `2023-09`,
+                institution_en: `Cornell University, Electrical and Computer Engineering Department
+                Photonics and Quantum Electronics Group`,
+                institution_ja: `コーネル大学 Electrical and Computer Engineering Department,
+                Photonics and Quantum Electronics Group`,
+                title_en: `Internship`,
+                title_ja: `インターンシップ`,
+                description_en: `Supervisor: Karan Mehta
+                Designed and simulated photonic crystal UV resonators in a novel platform via full 3D FDTD simulations`,
+                description_ja: `指導教員：Karan Mehta
+                紫外線領域での、フォトニック結晶光共振器の設計と3D-FDTDシミュレーション`,
+                link: `https://sites.coecis.cornell.edu/mehta/`
+        },
+        {
+            index: 3,
+            start_date: `2022-04`,
+            end_date: `2024-03`,
+            institution_en: `University of Tokyo, Faculty of Engineering, School of Applied Physics`,
+            institution_ja: `東京大学工学部物理工学科`,
+            title_en: `Senior Thesis`,
+            title_ja: `卒業研究`,
+            description_en: `Supervisor: Kosuke Yoshioka
+    Efficient detection of Ionized Positronium using MCP through Electromagnetic Field Simulations and Modifications`,
+            description_ja: `指導教員：吉岡孝高
+    電磁場シミュレーションと電極設計による、イオン化ポジトロニウムの効率的な検出方法の開発`,
+            link: `http://www.fs.t.u-tokyo.ac.jp/index.html`
+        },
 ];
 
 export const industrialExperienceItems = [
