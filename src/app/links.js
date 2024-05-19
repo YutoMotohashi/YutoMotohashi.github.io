fd4610c5b501d9abfe654a25d616d23f75745fdd
YutoMotohashi <88755943+YutoMotohashi@users.noreply.github.com>
2024-05-19 12:17:59 +0900

    Update links.js 大学院受験記のリンク追加

--- src/app/links.js
+++ src/app/links.js
@@ -18,6 +18,17 @@ function Links({ language }) {
                     <p>ETH Zurichに一年間交換留学した時に関連する情報はこちら</p>
                 </a>
                 <a
+                    href="https://xplane.jp/enroll2024-1-motohashi/"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        大学院受験記 <span>-&gt;</span>
+                    </h2>
+                    <p>XPLANEに寄稿した大学院受験記はこちら</p>
+                </a>
+                <a
                     href="https://www.utokyo-ipc.co.jp/ja/story/internship-yanekara-motohashi/"
                     className={styles.card}
                     target="_blank"
