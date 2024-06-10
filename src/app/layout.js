63284bf78c8d6fe3bf84ee2bf7c9837a3631565d
YutoMotohashi <88755943+YutoMotohashi@users.noreply.github.com>
2024-06-10 15:31:55 +0900

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Yuto Motohashi | 本橋悠人",
-  description: "Official webpage of Yuto Motohashi | 本橋悠人の公式ウェブサイトです",
+  description: "Official webpage of Yuto Motohashi. Discover the work of Yuto Motohashi, a quantum engineer specializing in trapped ion quantum computers. Learn about my research, publications, and contributions to quantum computing. | 本橋悠人の公式ウェブサイトです。イオントラップ方式の量子コンピュータを専門とする量子エンジニアです。研究内容、発表論文をご覧ください",
 };
 
 export default function RootLayout({ children }) {
