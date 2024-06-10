99e05aec59aff618e7d68580feb51b193877779f
YutoMotohashi <88755943+YutoMotohashi@users.noreply.github.com>
2024-06-10 15:49:29 +0900

    Update cv.js

--- src/app/cv.js
+++ src/app/cv.js
@@ -5,7 +5,6 @@ function CV({ language }) {
     const cv_pdf = "https://drive.google.com/file/d/171B8DPBi9K2v1HIKnJlxDC0ZLOP0w4Dp/view";
     return (
         <>
-            // <ComingSoon />
             {language === 'en' ? (
                 <div className="download-link">
                     <a href={cv_pdf} download="CV_Yuto_Motohashi.pdf">Download my CV from google drive</a>
